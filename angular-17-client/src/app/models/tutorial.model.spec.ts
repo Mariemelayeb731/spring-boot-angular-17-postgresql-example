91067070751e0dffa1196105dae231c0767104e5
mariem el ayeb <maryemayeb43@gmail.com>
2025-04-12 21:24:34 +0100

    Fix

--- angular-17-client/src/app/models/tutorial.model.spec.ts
+++ angular-17-client/src/app/models/tutorial.model.spec.ts
@@ -1,7 +1,10 @@
+// src/app/models/tutorial.model.spec.ts
+
 import { Tutorial } from './tutorial.model';
 
 describe('Tutorial', () => {
   it('should create an instance', () => {
-    expect(new Tutorial()).toBeTruthy();
+    const tutorial = new Tutorial('Test Tutorial', 'This is a test description', true);
+    expect(tutorial).toBeTruthy();
   });
 });
