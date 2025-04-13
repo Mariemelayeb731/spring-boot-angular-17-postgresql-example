03ca5f49044ff5f9a777eb7229eaf08c0c90205f
mariem el ayeb <maryemayeb43@gmail.com>
2025-04-13 07:17:57 +0100

    Ajout

--- angular-17-client/cypress.config.js
+++ angular-17-client/cypress.config.js
@@ -2,8 +2,6 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:4200', // Remplace par l'URL de ton application si différente
-    specPattern: 'cypress/e2e/**/*.cy.js', // Emplacement des tests
-    supportFile: false, // Si tu n'utilises pas de fichier de support global
-  },
+    baseUrl: 'http://localhost:4200',
+  }
 });
