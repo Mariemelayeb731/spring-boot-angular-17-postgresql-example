120b6a042c922b677c93b419a27e9c5cfcf3ca6f
mariem el ayeb <maryemayeb43@gmail.com>
2025-05-09 12:50:04 +0100

    Description des modifications apportées

--- angular-17-client/src/app/services/tutorial.service.spec.ts
+++ angular-17-client/src/app/services/tutorial.service.spec.ts
@@ -36,7 +36,7 @@ describe('TutorialService', () => {
       expect(tutorials).toEqual(dummyTutorials);
     });
 
-    const req = httpMock.expectOne('http://localhost:8084/api/tutorials');
+    const req = httpMock.expectOne('http://localhost:808O/api/tutorials');
     expect(req.request.method).toBe('GET');
     req.flush(dummyTutorials);
   });
@@ -48,7 +48,7 @@ describe('TutorialService', () => {
       expect(result).toEqual(tutorial);
     });
 
-    const req = httpMock.expectOne('http://localhost:8084/api/tutorials/1');
+    const req = httpMock.expectOne('http://localhost:8080/api/tutorials/1');
     expect(req.request.method).toBe('GET');
     req.flush(tutorial);
   });
